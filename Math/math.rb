7dc78a72e6f8502f080f9b937d5343c055a7093d
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-01-08 02:39:55 -0300

    Create math.rb

---
+++ Math/math.rb
@@ -0,0 +1,7 @@
+puts Math::PI #Pi
+puts Math::E #Neperiano
+puts Math.sin(0) #Seno de 0
+puts Math.cos(0) #Cosseno de 0
+puts Math.sqrt(81) #Raiz quadrada de 81
+puts (81**0.5).to_s #Raiz quadrada de 81^0,5
+puts Math.hypot(3,4) #Hipotenusa de 3 e 4 =5
